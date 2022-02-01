b9997dcd6ac9c70f24e566403157acc5a20bb93b
David Carlier <devnexen@gmail.com>
2022-02-01 16:14:53 +0000

    solarish systems dl_iterate_phdr support

--- src/unix/solarish/mod.rs
+++ src/unix/solarish/mod.rs
@@ -2826,6 +2826,19 @@ extern "C" {
         sfvcnt: ::c_int,
         xferred: *mut ::size_t,
     ) -> ::ssize_t;
+    // #include <link.h>
+    /*
+    pub fn dl_iterate_phdr(
+        callback: ::Option<
+            unsafe extern "C" fn(
+                info: *mut dl_phdr_info,
+                size: usize,
+                data: *mut ::c_void,
+            ) -> ::c_int,
+        >,
+        data: *mut ::c_void,
+    ) -> ::c_int;
+    */
     pub fn getpagesize() -> ::c_int;
     pub fn getpagesizes(pagesize: *mut ::size_t, nelem: ::c_int) -> ::c_int;
     pub fn mmapobj(
@@ -2873,5 +2886,8 @@ cfg_if! {
     if #[cfg(target_arch = "x86_64")] {
         mod x86_64;
         pub use self::x86_64::*;
+    } else if #[cfg(target_arch = "x86")] {
+        mod x86;
+        pub use self::x86::*;
     }
 }
